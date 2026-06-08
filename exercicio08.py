4272ac2bdb3cb3c71afc7bee651b82c6e9e54e3f
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-06-08 12:22:41 -0300

    Conclui exercicio 8 da lista

---
+++ exercicio08.py
@@ -0,0 +1,6 @@
+salario = float(input("Insira o valor do seu salário em reais: "))
+aumento = salario * 0.15
+novo_salario = salario + aumento
+
+print(f"O valor do aumento é R$ {aumento:.2f}")
+print(f"O novo salário é R$ {novo_salario:.2f}")
