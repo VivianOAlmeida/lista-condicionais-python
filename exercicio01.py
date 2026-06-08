18f9d448d26adac6dfd1b788d312ebf5a61bad98
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-06-08 08:34:50 -0300

    Conclui exercicio 1 da lista

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+numero = int(input("Insira um número inteiro: "))
+
+if numero > 0:
+    print("Seu número é positivo")
+elif numero == 0:
+    print("Impossível determinar se é positivo ou negativo pois seu número é neutro")
+else:
+    print("Seu número é negativo")
