9b861f6ef261e3eaca97854c8e9e590b80e2848f
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-06-08 12:20:22 -0300

    Conclui exercicio 3 da lista

---
+++ exercicio03.py
@@ -0,0 +1,9 @@
+numero1 = float(input("Insira o 1° número: "))
+numero2 = float(input("Insira o 2° número: "))
+
+if numero1 > numero2:
+    print("O 1° numero é maior.")
+elif numero1 == numero2:
+    print("Os dois números são iguais.")
+else:
+    print("O 2° número é maior.")
