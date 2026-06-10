fdf6b352ccd35dd044efe1467506fe86326ab792
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-06-10 08:47:01 -0300

    Conclui exercicio 15 da lista

---
+++ exercicio15.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite um numero inteiro: "))
+
+if numero >= 4 and numero <= 1532:
+    print("Seu número está dentro do intervalo númérico (4 - 1532)")
+else:
+    print("Seu número não faz parte do intervalo numérico (4 - 1532)")
