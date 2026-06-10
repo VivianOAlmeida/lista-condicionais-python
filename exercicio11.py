25c28dd612deb5140760295f3481c2e130ed5b24
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-06-10 08:09:42 -0300

    Conclui exercicio 11 da lista

---
+++ exercicio11.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Insira sua nota: "))
+nota2 = float(input("Insira sua nota: "))
+media = (nota1 + nota2) / 2
+
+if media >= 7:
+    print("Você foi aprovado.")
+else:
+    print("Você foi reprovado.")
