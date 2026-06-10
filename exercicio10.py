84657cf1c9af63699043b7ae6096188e817d6ea9
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-06-10 08:03:20 -0300

    Conclui exercicio 10 da lista

---
+++ exercicio10.py
@@ -0,0 +1,7 @@
+senha = str(input("Crie uma senha: "))
+entrada = str(input("Digite a sua senha: "))
+
+if senha == entrada:
+    print("Senha valida! Acesso permitido.")
+else:
+    print("Senha invalida! Acesso negado.")
